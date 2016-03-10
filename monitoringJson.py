af29925854a3345bf6c7c75085e488def55f2c17
diwaakartg@gmail.com <diwaakartg@gmail.com>
2016-03-10 21:19:37 +0900

    regex

--- monitoringJson.py
+++ monitoringJson.py
@@ -21,51 +21,76 @@ monitProcess = subprocess.Popen(['monit', 'status'], stdout=subprocess.PIPE, std
 output, error = monitProcess.communicate()
 
 
+# The output looks like this. Now need to extract the needed value using regular expression.
+
+    # Process 'nginx'
+    #  status                            Running
+    #  monitoring status                 Monitored
+    #  pid                               1033
+    #  parent pid                        1
+    #  uid                               0
+    #  effective uid                     0
+    #  gid                               0
+    #  uptime                            1h 4m
+    #  children                          1
+    #  memory                            5.5 MB
+    #  memory total                      11.3 MB
+    #  memory percent                    1.1%
+    #  memory percent total              2.3%
+    #  cpu percent                       0.0%
+    #  cpu percent total                 0.0%
+    #  data collected                    Thu, 10 Mar 2016 12:14:24
+
+    # System ''
+    #  status                            Running
+    #  monitoring status                 Monitored
+    #  load average                      [0.07] [0.18] [0.18]
+    #  cpu                               0.0%us 0.1%sy
+    #  memory usage                      70.3 MB [14.4%]
+    #  swap usage                        0 B [0.0%]
+    #  data collected                    Sun, 06 Mar 2016 05:56:10
+
+
+# Extracting the process name
 processLineRegex = re.findall(r'Process\s.*', output)
 processExtractArray = [];
 for process in processLineRegex:
     intermediate = (re.sub('Process \'', '', process))
     processExtractArray.append(re.sub('\'', '', intermediate))
 
-print processExtractArray
-
+# Extracting the status of the service
 statusLineRegex = re.findall(r'Process\s.*\n\s*status.*', output)
 statusExtractArray = [];
 for status in statusLineRegex:
     statusExtractArray.append(re.sub(r'Process\s.*\n\s*status\s*', '', status))
 
-print statusExtractArray
-
+# Extracting the pid of the service
 pidLineRegex = re.findall(r'\n\s*pid.*', output)
 pidExtractArray = []
 for pid in pidLineRegex:
     pidExtractArray.append(re.sub(r'\D', '', pid))
 
-print pidExtractArray
-
+# Extracting the memory of the service
 memoryTotalLineRegex = re.findall(r'memory\stotal.*', output)
 memoryTotalExtractArray = []
 for mt in memoryTotalLineRegex:
     memoryTotalExtractArray.append(re.sub(r'memory\stotal\s*', '', mt))
 
-print memoryTotalExtractArray
-
+# Extracting the memory percentage of the service
 memoryPercentTotalLineRegex = re.findall(r'memory\spercent\stotal.*', output)
 memoryPercentTotalExtractArray = []
 for mpt in memoryPercentTotalLineRegex:
     intermediate = re.sub(r'memory\spercent\stotal\s*', '', mpt)
     memoryPercentTotalExtractArray.append(re.sub(r'%', '', intermediate))
 
-print memoryPercentTotalExtractArray
-
+# Extracting the cpu percentage of the service
 cpuPercentTotalLineRegex = re.findall(r'cpu\spercent\stotal.*', output)
 cpuPercentTotalExtractArray = []
 for cpt in cpuPercentTotalLineRegex:
     intermediate = re.sub(r'cpu\spercent\stotal\s*', '', cpt)
     cpuPercentTotalExtractArray.append(re.sub(r'%', '', intermediate))
 
-print cpuPercentTotalExtractArray
-
+# creating the json
 monitoringJson['message'] = {}
 for service in range(0, len(processExtractArray)):
     processJson = {}
@@ -76,19 +101,7 @@ for service in range(0, len(processExtractArray)):
     processJson['cpuPercent'] = cpuPercentTotalExtractArray[service]
     monitoringJson['message'][processExtractArray[service]] = processJson
 
-# print monitoringJson
-
-# The output looks like this. Now need to extract the needed value using regular expression.
-
-# System ''
-#  status                            Running
-#  monitoring status                 Monitored
-#  load average                      [0.07] [0.18] [0.18]
-#  cpu                               0.0%us 0.1%sy
-#  memory usage                      70.3 MB [14.4%]
-#  swap usage                        0 B [0.0%]
-#  data collected                    Sun, 06 Mar 2016 05:56:10
-
+# For system monitaring details.
 # Extracting the load average.
 # LoadAverage is used to find the idle state, no. of processes waiting for the resources.
 # LoadAverage is generaaly given by the uptime command.
@@ -171,10 +184,7 @@ systemMonitoringJson['swapUsagePercentage'] = swapUsagePercentage
 monitoringJson['message']['system'] = systemMonitoringJson
 
 # Encoding as a json using the json module.
-systemMonitoringJsonEncoding = json.dumps(systemMonitoringJson)
-
-print monitoringJson
-
+monitoringJsonEncoding = json.dumps(monitoringJson)
 
 # Printing the systemMonitoringJson.
-print systemMonitoringJsonEncoding
+print monitoringJsonEncoding
