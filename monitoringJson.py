e6338ce602e40b6b00809a70f9ab6682f55d8542
diwaakartg@gmail.com <diwaakartg@gmail.com>
2016-03-10 20:26:25 +0900

    regex

--- monitoringJson.py
+++ monitoringJson.py
@@ -36,7 +36,7 @@ print statusExtractArray
 pidLineRegex = re.findall(r'pid.*', output)
 pidExtractArray = []
 for pid in pidLineRegex:
-    pidExtractArray.append(re.sub(r'pid\s*', '', pid))
+    pidExtractArray.append(re.sub(r'\n\s*pid\s*', '', pid))
 
 print pidExtractArray
 # The output looks like this. Now need to extract the needed value using regular expression.
